5234bcd4a886b9375f2f8755a28de111208cec2c
SebasSalazar <sebajuan3794@gmail.com>
2023-07-25 22:19:28 -0500

    se realizaron ejercicios de javascript

--- Javascript/ejercicio2/js/script2.js
+++ Javascript/ejercicio2/js/script2.js
@@ -6,7 +6,7 @@ let nsueldo = parseInt(sueldo) + parseInt(sueldo*psueldo)
 
 alert("El nuevo sueldo con el aumento es: $" +nsueldo+ "COP") */
 
-/*ejercicio 3 - un vendedor que tiene su sueldo base recive una comision del 30% del total de las ventas del mes, el quiere saver cuanto ganara en un mes que tuvo 3 ventas*/
+/*ejercicio 3 - un vendedor que tiene su sueldo base recive una comision del 30% del total de las ventas del mes, el quiere saver cuanto ganara en un mes que tuvo 3 ventas
 let sueldo = prompt("Ingrese el valor del suedo");
 let venta1 = prompt("Ingrese el valor de la venta");
 let venta2 = prompt("Ingrese el valor de la venta");
@@ -16,4 +16,28 @@ let comision = parseInt(venta1)+ parseInt(venta2)+ parseInt(venta3);
 let resul = parseInt(sueldo) + parseInt(comision*porc);
 let resulf = parseInt(resul); 
 
-alert("Su sueldo junto a la comision por las 3 ventas es de $" +resulf+ " COP");
+alert("Su sueldo junto a la comision por las 3 ventas es de $" +resulf+ " COP");*/
+
+/*ejercicio 4 - Una tienda ofrece un 25% de descuento por cada compra. Un cliente desea
+saber cuánto deberá pagar por su compra
+let p1 = prompt("ingrese valor del producto")
+let p2 = prompt("ingrese valor del producto")
+let p3 = prompt("ingrese valor del producto")
+let porc = 25/100;
+let porcp1 = parseInt(p1) - parseInt(p1*porc)
+let porcp2 = parseInt(p2) - parseInt(p2*porc)
+let porcp3 = parseInt(p3) - parseInt(p3*porc)
+let pagof = parseInt(porcp1+porcp2+porcp3)
+alert("El valor a pagar por cada porducto es de: "+pagof)*/
+
+
+/*ejercicio 5 - Suponga que un individuo desea invertir su capital en un banco y desea saber
+cuánto dinero ganara después de un mes si el banco paga a razón de 2%
+mensual*/
+
+let inv = prompt("ingre v alor de inversion")
+let porc = 2/100;
+let gana = parseInt(inv*porc)
+alert("al final del mes obtuvo una ganacia de " +gana)
+
+
